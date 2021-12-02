ba4582c191d4ecf1d0ef0e218fa62b8dcded2c5a
santialvarez0207 <68878932+santialvarez0207@users.noreply.github.com>
2021-12-01 21:16:06 -0500

    conexion del registro admin con la base de dato

--- src/Frontend/src/app/date-admon/date-admon.component.ts
+++ src/Frontend/src/app/date-admon/date-admon.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ControlContainer, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {administrador} from '../models/administrador'
 
 @Component({
   selector: 'app-date-admon',
@@ -7,10 +8,17 @@ import { ControlContainer, FormBuilder, FormControl, FormGroup, Validators } fro
   styleUrls: ['./date-admon.component.css']
 })
 export class DateAdmonComponent implements OnInit {
+   
+  admins: administrador[] = []
+  constructor() { }
 
+  ngOnInit(): void {
+    this.obtenerAdmin()
+  }
   formExample = new FormGroup({
 
     name: new FormControl('',[Validators.required]),
+    lastName: new FormControl('',[Validators.required]),
     email: new FormControl('',[Validators.required,Validators.email]),
     password: new FormControl('',[Validators.required]),
     departament: new FormControl('',[Validators.required]),
@@ -18,23 +26,60 @@ export class DateAdmonComponent implements OnInit {
     number: new FormControl('',[Validators.required]),
     Identification: new FormControl('',[Validators.required]),
     address: new FormControl('',[Validators.required]),
+    edad: new FormControl('',[Validators.required]),
+    genero: new FormControl(''),
 
-});
-
-  constructor() {
-    
-
-    
-   }
-
-  ngOnInit(): void {
-    /*
-
-    */
-  }
+  });
 
   send():any{
-    console.log(this.formExample)
+    let email:string = this.formExample.value.email
+    
+    this.obtenerAdmin()
+    
+    let resultado = this.admins.find(function(element) {
+      return element.correo == email;
+    });
+    
+    if (resultado) {
+      console.log("el usuario ya existe")
+    }else{
+      this.registrarAdmin()
+    }
+    console.log(this.admins)
+    this.formExample.reset()
+  }
+  
+  obtenerAdmin(): void{
+    
+    let url = "http://localhost:3000/administradors"
+    fetch(url).then(response => response.json())
+    .then(data => {
+      this.admins = data
+    })
+
   }
 
+  registrarAdmin(): void{
+    let url = "http://localhost:3000/administradors"
+    let date = {
+      nombre: this.formExample.value.name,
+      apellido: this.formExample.value.lastName,
+      celular: this.formExample.value.number,
+      correo: this.formExample.value.email,
+      contrasena: this.formExample.value.password,
+      genero: this.formExample.value.genero,
+      edad: this.formExample.value.edad
+    }
+    
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(date),
+      headers: {
+        'content-type': 'application/json'
+      }
+    }).then(res => res.json())
+    .then(mensaje => {
+      console.log(mensaje)
+    })
+  }
 }
---
+++ src/Frontend/src/app/models/administrador.ts
@@ -0,0 +1,12 @@
+
+
+export interface administrador{
+    nombre: String,
+    apellido: String,
+    celular: String,
+    correo: String,
+    contrasena: String,
+    genero: String,
+    edad: String
+}
+
