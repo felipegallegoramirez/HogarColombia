44a36de9a3b73cc9987e97d175a9944cdb635b7e
Felipe Gallego <felipegallego600@gmail.com>
2021-12-08 09:40:53 -0500

    randi

---
+++ src/Backend/src/config/keys.ts
@@ -0,0 +1,9 @@
+export namespace Keys {
+    export const carpetaImagen = '../../archivos/imagenes';
+    export const nombreCampoImagen = 'file';
+    export const extensionesPermitidasIMG: string[] = ['.PNG', '.JPG', '.JPEG', '.SVG'];
+    export const tamMaxImagenVehiculo = 10024 * 10024;
+    export const carpetaDocumentoPersona = '../../archivos/documentos';
+    export const nombreCampoDocumentoPersona = 'file';
+    export const extensionesPermitidasDOC: string[] = ['.PDF', '.DOC', '.DOCX', '.XLS', '.XLSX'];
+  }
