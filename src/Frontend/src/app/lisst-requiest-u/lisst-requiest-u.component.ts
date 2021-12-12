3c5cb2d701261040a12afb2f383adab422eb5d91
Felipe Gallego <felipegallego600@gmail.com>
2021-12-12 09:46:45 -0500

    filtro

--- src/Frontend/src/app/lisst-requiest-u/lisst-requiest-u.component.ts
+++ src/Frontend/src/app/lisst-requiest-u/lisst-requiest-u.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { request } from '../models/request';
+import { Inmueble } from '../models/inmueble';
 
 @Component({
   selector: 'app-lisst-requiest-u',
@@ -9,8 +10,18 @@ import { request } from '../models/request';
 export class LisstRequiestUComponent implements OnInit {
 
   constructor() { }
-
-  ngOnInit(): void {
+  cat: Array<request> = [];
+  rar: Array<Inmueble> = [];
+  async ngOnInit(): Promise<void> {
+    let data= localStorage.getItem('persona')
+    //@ts-ignore
+    let asesor=JSON.parse(data)
+    this.cat = await this.obtenerSolisitudes(asesor.id);
+    for (let i=0;i>=this.cat.length-1;i++){
+      this.rar[i]= await this.obtenerInmueble(this.cat[i].inmuebleId)
+    }
+    console.log(this.rar)
+    
   }
 
   
@@ -21,5 +32,16 @@ export class LisstRequiestUComponent implements OnInit {
     const datos = await respuesta.json() as request[]
     return datos
   }
+
+  
+  async obtenerInmueble(id:string): Promise<Inmueble> {
+    let url = 'http://localhost:3000/inmuebles/'+id;
+    const respuesta = await fetch(url);
+    const datos = (await respuesta.json()) as Inmueble;
+    return datos;
+  }
+
+
+  
 
 }
--- src/Frontend/src/app/search/search.component.ts
+++ src/Frontend/src/app/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { filter } from 'rxjs';
 import { AppComponent } from '../app.component';
 import{ Inmueble } from '../models/inmueble'
 
@@ -8,24 +9,17 @@ import{ Inmueble } from '../models/inmueble'
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-  type:string;
-  price:string;
-  state:string;
-  ubication:string;
-  size:string;
+  size:number;
 
   constructor() {
-    this.type="Apartament"
-    this.state="Sale"
-    this.price="123124143"
-    this.ubication="Medellin"
-    this.size="128"
+    this.size=128
    }
 
    cat: Array<Inmueble> = [];
 
    async ngOnInit(): Promise<void> {
     this.cat = await this.obtenerCateogry();
+    this.size = this.cat.length
   }
 
 
@@ -40,4 +34,33 @@ export class SearchComponent implements OnInit {
     const datos = (await respuesta.json()) as Inmueble[];
     return datos;
   }
+
+  public reset: any[] = [{}];
+
+  async onRecreate(): Promise<void> {
+
+      this.cat = [];
+      let asd = await this.obtenerCateogry();
+      this.cat= asd.filter(this.filtro)
+      this.size = this.cat.length
+  }
+
+  filtro(asd:Inmueble){
+
+    let a = <HTMLInputElement>document.querySelector('#city')
+    let b = <HTMLInputElement>document.querySelector('#type')
+    let c = <HTMLInputElement>document.querySelector('#ofert')
+
+    
+
+  let g= false
+  if ((asd.categoria==c.value || c.value=="")&&(asd.ciudad==a.value || a.value=="")&&(asd.tipoOferta==b.value || b.value=="")){
+    g=true;
+    console.log(a.value , b.value , c.value)
+  }
+  
+  return g
+
+  }
+
 }
